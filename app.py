9dce34e10953941b56ad93b561254eebfcee3591
Henry Marquardt <hmarquardt@berkeley.edu>
2024-03-22 13:37:00 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import os
+import cv2
+import io
+import numpy as np
+from flask import Flask, request, render_template, send_file, jsonify
+from PIL import Image
+from mymodels import poseEstimationModule as pem
+
+
+app = Flask(__name__)
+
+# Ensure there's a folder for uploaded images
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return 'No file part', 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        filename = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filename)
+
+        try:
+            # Process the image with the pose estimation model
+            img = cv2.imread(str(filename))
+            detector = pem.poseDetector()
+            img = detector.findPose(img)
+            img = detector.findFeet(img)
+
+            # apply color correction
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            # Convert NumPy array to PIL Image
+            img = Image.fromarray(np.uint8(img))
+            
+            # Convert PIL Image to byte stream
+            img_io = io.BytesIO()
+            img.save(img_io, format='JPEG', quality=70)
+            img_io.seek(0)
+            
+            # remove the downloaded image after processing to avoid build up
+            os.remove(filename)
+
+            return send_file(img_io, mimetype='image/jpeg')
+
+        except FileNotFoundError:
+            return jsonify({'error': 'File not found after saving'}), 500
+        except IOError:
+            return jsonify({'error': 'Error processing image file'}), 500
+        except Exception as e:
+            # Catch all for any other errors during processing
+            return jsonify({'error': 'Error during image processing', 'message': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mymodels/poseEstimationModule.py
@@ -0,0 +1,86 @@
+import cv2
+import mediapipe as mp
+import time
+import os
+
+class poseDetector:
+    def __init__(self, mode = False, upBody = False, smooth = True, detectionConf = 0.5, trackingConf = 0.5 ) -> None:
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionConf = detectionConf
+        self.trackingConf = trackingConf
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, 
+                                     min_detection_confidence=self.detectionConf, 
+                                     min_tracking_confidence=self.trackingConf)
+
+
+    def findPose(self, img, draw = True):
+        """Find the pose and draw lines/dots on the image (set draw = False to disable)"""
+
+        # send image to nn, receive list of coordinates for all 33 body landmarks (nose, hands, feet, etc.)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        
+        # if any body parts are foung in the image, draw them 
+        if draw and self.results.pose_landmarks:
+            self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
+        return img
+
+
+    def findFeet(self, img, draw=True):
+        """
+        :draw:
+            - True = draws points on image_array
+            - False = does not alter image, returns feet coordinates -> list([id, x, y], [id, x, y], ...)
+        """
+
+        feetList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                # get dimensions of the img frame
+                h, w, c, = img.shape
+                # print(id, lm)
+
+                if id in [27, 28, 29, 30, 31, 32]:
+                    # convert landmark coordinates from percentage of width and height to pixel coordinates
+                    cx, cy = int(lm.x*w), int(lm.y*h)
+
+                    # store coordinates in return list
+                    feetList.append([id, cx, cy])
+
+                    # draw cirle, if applicable
+                    if draw:
+                        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        if draw:
+            return img
+        else:
+            return feetList
+
+
+def main():
+    cap = cv2.VideoCapture(os.path.join('Videos', 'processed', 'putts1.mp4'))
+    pTime = 0
+
+    detector = poseDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+
+        
+        cv2.imshow("Image", img)
+        # cv2.putText(img, text=str(int(fps)), org = (70, 50), fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 3, color = (255, 0, 0), thickness = 3)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
